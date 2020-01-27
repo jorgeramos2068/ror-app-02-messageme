9e250af90a94e7aae579bace53c05fa6faef8c69
Jorge Ramos <jorgeramos2068@gmail.com>
2020-01-27 12:35:44 -0700

    Add authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # Root
   root 'chatroom#index'
-  # Login
+
+  # Login and logout
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
