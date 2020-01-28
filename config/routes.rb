82910ac47e9bceb7a5fcdcc9605233d7d2fd334c
Jorge Ramos <jorgeramos2068@gmail.com>
2020-01-28 12:43:33 -0700

    Create functionality to add messages on chat

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  # Messages
+  post 'message', to: 'messages#create'
 end
