1ca90af57b34e0ea82f5499cc51a7ebf15397a48
Jorge Ramos <jorgeramos2068@gmail.com>
2020-02-04 14:07:34 -0700

    Add Chatroom channel using ActionCable

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,7 +1,12 @@
 $(document).on('turbolinks:load', function () {
+  // Dropdown menu
   $('.ui.dropdown').dropdown();
 
+  // Closing message alerts
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
+
+  // Scroll bottom function
+
 });
